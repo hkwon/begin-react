5b593778f5ee151fdab4b3b62994f3fa96a8858d
hkwon <mr.hyuk.kwon@gmail.com>
2019-08-20 21:00:34 +0900

    keep studing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useMemo } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
@@ -74,6 +74,13 @@ function App() {
       : user));
   }
 
+  const countActiveUsers = (users) => {
+    console.log('활성 사용자 수를 세는중...');
+    return (users.filter(user => user.active).length);
+  }
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
+
   return (
     <>
       <CreateUser 
@@ -83,6 +90,7 @@ function App() {
         onCreate={onCreate} 
       />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성 사용자 수: {count}</div>
     </>
   );
 }
